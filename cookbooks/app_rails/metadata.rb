31f13932b6d9016d5e7912c47ca521293f31368f
caryp <cary@rightscale.com>
2009-07-20 17:11:59 -0700

    updated metadata and config files

--- cookbooks/app_rails/metadata.rb
+++ cookbooks/app_rails/metadata.rb
@@ -8,27 +8,15 @@ attribute "rails",
   :display_name => "rails hash",
   :description => "hash of rails attributes",
   :type => "hash"
-
+  
 attribute "rails/code",
   :display_name => "rails application code hash",
   :description => "hash of rails code attributes",
   :type => "hash"
 
-attribute "rails/code/url",
-  :display_name => "repository url",
-  :description => "location of application code repository",
-  :required => true
-
-attribute "rails/code/user",
-  :display_name => "repository username",
-  :description => "username for code repository",
-  :required => true
-
-attribute "rails/code/creditials",
-  :display_name => "repository credentials",
-  :description => "credentials for code repository",
-  :required => true
-
+#
+# required
+#
 attribute "rails/db_app_user",
   :display_name => "database user",
   :description => "username for database access",
@@ -49,9 +37,66 @@ attribute "rails/db_dns_name",
   :description => "FQDN of the database server",
   :required => true
 
+
+attribute "rails/code/url",
+  :display_name => "repository url",
+  :description => "location of application code repository",
+  :required => true
+
+attribute "rails/code/user",
+  :display_name => "repository username",
+  :description => "username for code repository",
+  :required => true
+
+attribute "rails/code/creditials",
+  :display_name => "repository credentials",
+  :description => "credentials for code repository",
+  :required => true
+
+#
+# recommended
+#
+attribute "rails/server_name",
+  :display_name => "server name",
+  :description => "FQDN for the server",
+  :default => "myserver"
+
+attribute "rails/application_name",
+  :display_name => "application name",
+  :description => "give a name to your application",
+  :default => "myapp"
+
+attribute "rails/env",
+  :display_name => "rails environment",
+  :description => "production, test, staging, etc.",
+  :default => "production"
+
+#
+# optional
+#
+attribute "rails/code/destination",
+  :display_name => "application home",
+  :description => "what directory do you want your application to live in?",
+  :default => "/home/webapp/#{rails[:application_name]}"
+
+attribute "rails/code/branch",
+  :display_name => "repository branch",
+  :description => "branch to pull source from",
+  :default => "master"
+  
+attribute "rails/application_port",
+  :display_name => "application port",
+  :description => "the port your rails application will listen on",
+  :default => "8000"
+
+attribute "rails/spawn_method",
+  :display_name => "spawn method",
+  :description => "what spawn method should we use?",
+  :default => "conservative"
+
 attribute "rails/opt_gems_list",
   :display_name => "opt_gems_list",
-  :description => "optional list of gems to install",
+  :description => "list of gems required by your application",
   :type => "array"
   :required => false
 
