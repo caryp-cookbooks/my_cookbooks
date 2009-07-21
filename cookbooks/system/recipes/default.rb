82fdba612e5b6e558fcf98dbc64b0acb938694b7
root <root@domU-12-31-39-00-66-56.compute-1.internal>
2009-07-21 20:57:06 +0000

    added syslog

--- cookbooks/system/recipes/default.rb
+++ cookbooks/system/recipes/default.rb
@@ -24,7 +24,28 @@ execute "ensure_dev_null" do
   creates "/dev/null.syslog-ng"
   command "mknod /dev/null.syslog-ng c 1 3"
 end
-template @node[:system][:logrotate_config] do
+template "/etc/syslog-ng/syslog-ng.conf" do
   source "syslog-ng.conf"
 end
+service "syslog-ng" do
+  supports :start => true, :stop => true, :restart => true
+  action [ :enable, :restart ]
+end
+bash "configure_logrotate_for_syslog" do 
+  code <<-EOH
+    perl -p -i -e 's/weekly/daily/; s/rotate\s+\d+/rotate 7/' /etc/logrotate.conf
+    [ -z "$(grep -lir "missingok" #{@node[:system][:logrotate_config]}_file)" ] && sed -i '/sharedscripts/ a\    missingok' #{@node[:system][:logrotate_config]}_file
+    [ -z "$(grep -lir "notifempty" #{@node[:system][:logrotate_config]}_file)" ] && sed -i '/sharedscripts/ a\    notifempty' #{@node[:system][:logrotate_config]}_file
+  EOH
+end
+directory "/var/spool/oldmail" do 
+  recursive true 
+  mode "775"
+  owner "root"
+  group "mail"
+end
+remote_file "/etc/logrotate.d/mail" do 
+  source "mail"
+end
+
 
