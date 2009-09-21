cdaa0e06c412ed9a6e3c618b92286d1ddd25449c
erik <erik@erik-1763.local>
2009-09-21 15:40:15 -0700

    Quoted DME info

--- cookbooks/db_mysql/recipes/register_public_ip.rb
+++ cookbooks/db_mysql/recipes/register_public_ip.rb
@@ -8,8 +8,8 @@
 
 # configure master DB DNS record 
 dns @node[:db_mysql][:dns][:master_id] do
-  user @node[:db_mysql][:dns][:user]
-  passwd @node[:db_mysql][:dns][:password]
+  user "#{@node[:db_mysql][:dns][:user]}"
+  passwd "#{@node[:db_mysql][:dns][:password]}"
   ip_address @node[:cloud][:public_ip]
 end
 
