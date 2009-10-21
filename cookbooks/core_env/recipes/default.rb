2638401d200ee986b10fefa2c6e7eccd38842bbd
caryp <cary@rightscale.com>
2009-10-20 19:56:09 -0700

    added core_env cookbook

---
+++ cookbooks/core_env/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: core_env
+# Recipe:: default
+#
+# Copyright 2009, RightScale, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "core_env::do_write_to_file"
