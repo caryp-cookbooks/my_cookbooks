4029f7515693e098f7a4f542d5b431f51b1a1901
caryp <cary@rightscale.com>
2009-10-27 15:02:01 -0700

    ohai_test: added failure checks

--- cookbooks/ohai_test/recipes/do_write_to_file.rb
+++ cookbooks/ohai_test/recipes/do_write_to_file.rb
@@ -11,3 +11,6 @@ template "/tmp/ohai_values.log" do
   source "ohai_values.erb"
   action :create
 end
+
+raise "ERROR: ohai ruby_bin value: #{@node[:languages][:ruby][:ruby_bin]} != '/usr/bin/ruby1.8'" unless @node[:languages][:ruby][:ruby_bin] == '/usr/bin/ruby1.8'
+raise "ERROR: ohai gems_dir value: #{@node[:languages][:ruby][:gems_dir]} != '/usr/lib/ruby/gems/1.8'" unless @node[:languages][:ruby][:gems_dir] == '/usr/lib/ruby/gems/1.8'
