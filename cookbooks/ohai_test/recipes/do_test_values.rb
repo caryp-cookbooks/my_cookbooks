772ad55c8f3a939f5711848dfe1e4b719033acb4
caryp <cary@rightscale.com>
2009-10-27 16:48:36 -0700

    ohai_test: improve test audit logs

--- cookbooks/ohai_test/recipes/do_test_values.rb
+++ cookbooks/ohai_test/recipes/do_test_values.rb
@@ -7,8 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-test_failed = ( @node[:languages][:ruby][:ruby_bin] =~ /sandbox/ )
-raise "ERROR: ohai ruby plugin: 'ruby_bin' value points to sandbox: #{@node[:languages][:ruby][:ruby_bin]}" if test_failed
+ruby_block "ruby_bin should not point to sandbox" do
+  block do
+    test_failed = ( @node[:languages][:ruby][:ruby_bin] =~ /sandbox/ )
+    raise "ERROR: ohai ruby plugin: 'ruby_bin' value points to sandbox: #{@node[:languages][:ruby][:ruby_bin]}" if test_failed
+    Chef::Log.info("== PASS ==")
+  end
+end
 
-test_failed = ( @node[:languages][:ruby][:gems_dir] =~ /sandbox/ )
-raise "ERROR: ohai ruby plugin: 'gems_dir' value points to sandbox: #{@node[:languages][:ruby][:gems_dir]}" if test_failed
+ruby_block "gems_dir should not point to sandbox" do
+  block do
+   test_failed = ( @node[:languages][:ruby][:gems_dir] =~ /sandbox/ )
+   raise "ERROR: ohai ruby plugin: 'gems_dir' value points to sandbox: #{@node[:languages][:ruby][:gems_dir]}" if test_failed
+   Chef::Log.info("== PASS ==")
+  end
+end
