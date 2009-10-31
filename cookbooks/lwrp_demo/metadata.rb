fbc03828387ae5961c9b5534094e0a313ecb740a
caryp <cary@rightscale.com>
2009-10-31 12:13:11 -0700

    lwrp_demo: new cookbook

---
+++ cookbooks/lwrp_demo/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale, Inc."
+maintainer_email "cary@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures lwrp_demo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+depends "resource:repo" # not really in metadata spec yet 
---
+++ cookbooks/lwrp_demo/recipes/do_source_pull.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: lwrp_demo
+# Recipe:: do_source_pull
+#
+# Copyright 2009, RightScale, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+Chef::Log.info("do some stuff here")
+
+repo "get source code" do
+  destination "/tmp/demo"
+  action :pull
+end
+
+Chef::Log.info("do some other stuff here")
