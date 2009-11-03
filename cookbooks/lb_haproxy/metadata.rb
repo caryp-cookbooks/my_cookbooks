764248e06693e79d5d3248d8d384c4ae452cda65
caryp <cary@rightscale.com>
2009-11-02 20:28:22 -0800

    repository review

--- cookbooks/lb_haproxy/metadata.rb
+++ cookbooks/lb_haproxy/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@rightscale.com"
 license          "All rights reserved"
 description      "Installs/Configures lb_haproxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.0.1"
 
 recipe "lb_haproxy::default", "Installs haproxy"
 recipe "lb_haproxy::install_haproxy", "Installs haproxy"
--- cookbooks/rs_tools/metadata.rb
+++ cookbooks/rs_tools/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "support@rightscale.com"
 license          "All rights reserved"
 description      "Installs/configures RightScale premium tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.0.11"
 
 provides "rs_tools(:name)"
