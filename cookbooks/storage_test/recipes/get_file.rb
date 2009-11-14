30a06f9e4e764d833dd17c25b6660d19ddd3a9a7
root <root@ip-10-212-139-17.ec2.internal>
2009-11-14 01:26:28 +0000

    using new provider

--- cookbooks/storage_test/recipes/get_file.rb
+++ cookbooks/storage_test/recipes/get_file.rb
@@ -7,8 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_object_store "get a file" do
+remote_object_store "resource_one" do
   user "blah"
   key "blah"
+  provider_type @node[:remote_object_store][:provider_type] 
+  action :nothing
+end
+
+remote_object_store "resource_one" do
+  container "MyBucket"
+  object_name "file1.txt"
   action :get
 end
+
+remote_object_store "resource_one" do
+  object_name "file2.txt"
+  action :get
+end
+
