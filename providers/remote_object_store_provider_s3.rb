3cf459935253ee099b72048a1f1407f16ca1dc2f
root <root@ip-10-212-118-66.ec2.internal>
2009-11-17 00:31:21 +0000

    added resource and provider directories

---
+++ providers/remote_object_store_provider_s3.rb
@@ -0,0 +1,90 @@
+#
+# Copyright (c) 2009 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require "rubygems"
+require "right_aws"
+
+class Chef
+  class Provider
+    class RemoteObjectStoreS3 < Chef::Provider
+
+      def load_current_resource
+        true
+      end
+
+      def action_login
+        Chef::Log.debug "action:login #{@new_resource.name}"
+        interface = RightAws::S3Interface.new( @new_resource.user, @new_resource.key, {} )
+        ObjectRegistry.register(@node, @new_resource.name, interface)
+        true
+      end
+
+      def action_get
+        Chef::Log.debug "action_get: get #{@new_resource.object_name} from #{@new_resource.container} to the file #{@new_resource.name}"
+        get_file(get_or_create_interface, @new_resource.container, @new_resource.object_name, @new_resource.name)
+        true
+      end
+      
+      def action_put
+        Chef::Log.debug "action_get: put #{@new_resource.object_name} to #{@new_resource.container} from the file #{@new_resource.name}"
+        put_file(get_or_create_interface, @new_resource.container, @new_resource.object_name, @new_resource.name)
+        true
+      end
+      
+      def action_delete
+        Chef::Log.debug "action_delete: put #{@new_resource.object_name} from #{@new_resource.container}"
+        delete_file(get_or_create_interface, @new_resource.container, @new_resource.object_name)
+        true
+      end
+      
+    private
+          
+      def get_or_create_interface
+        interface = ObjectRegistry.lookup(@node, @new_resource.name)
+        unless interface
+          action_login
+          interface = ObjectRegistry.lookup(@node, @new_resource.name)
+        end
+        interface
+      end
+      
+      def get_file(interface, bucket, object_name, destination_file)
+        file = ::File.new(destination_file, ::File::CREAT|::File::RDWR)
+        interface.get(bucket, object_name) do |chunk|
+          file.write(chunk)
+        end
+        file.close
+      end
+          
+      def put_file(interface, bucket, object_name, source_file)
+        interface.put(bucket, object_name,  ::File.open(source_file))
+      end
+      
+      def delete_file(interface, bucket, object_name)
+        interface.delete(bucket, object_name)
+      end
+    
+    end
+  end
+end
+
+
---
+++ resources/remote_object_store_resource.rb
@@ -0,0 +1,87 @@
+#
+# Copyright (c) 2009 RightScale Inc
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+class Chef
+  class Resource
+
+    class RemoteObjectStore < Chef::Resource
+
+     def initialize(name, collection=nil, node=nil)
+        super(name, collection, node)
+        @resource_name = :remote_object_store
+        @user = nil
+        @key = nil
+        @container = nil
+        @object_name = nil    
+        @provider_type = nil
+        @allowed_actions.push(:get, :put, :delete, :login)
+      end
+
+      def user(arg=nil)
+        set_or_return(
+          :user,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def key(arg=nil)
+        set_or_return(
+          :key,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def container(arg=nil)
+        set_or_return(
+          :container,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def object_name(arg=nil)
+        set_or_return(
+          :object_name,
+          arg,
+          :kind_of => [ String ]
+        )
+      end
+
+      def provider_type(arg=nil)
+        set_or_return(
+            :provider_type,
+            arg,
+          { :equal_to => [ "S3", "CloudFiles" ], :required => true }
+        )
+	ptypes = { "S3" => Chef::Provider::RemoteObjectStoreS3 } #, :cf => RemoteObjectStoreCF}
+	provider ptypes[@provider_type]
+      end
+
+    end
+    
+  end
+end
+
+Chef::Platform.platforms[:default].merge!(:remote_object_store => Chef::Provider::RemoteObjectStoreS3)
+
