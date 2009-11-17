c5cd85de2bc0d6b8d9ba91a6fabf765fde503171
root <root@ip-10-212-118-66.ec2.internal>
2009-11-17 00:06:13 +0000

    storage_test: debuged and working

--- cookbooks/storage_test/recipes/test_s3.rb
+++ cookbooks/storage_test/recipes/test_s3.rb
@@ -7,20 +7,20 @@ STORAGE_TEST_FILE_PATH = "/tmp/storage_test"
 # create test file
 template "#{STORAGE_TEST_FILE_PATH}.orig" do
   source "test_file.erb"
-  variables {
-    :provider => STORAGE_TEST_PROVIDER,
-    :container => STORAGE_TEST_CONTAINER,
-    :object_name => STORAGE_TEST_OBJECT_NAME
-  }
+  variables ({
+    :provider => "#{STORAGE_TEST_PROVIDER}",
+    :container => "#{STORAGE_TEST_CONTAINER}",
+    :object_name => "#{STORAGE_TEST_OBJECT_NAME}"
+  })
 end
 
 # upload to S3
 remote_object_store "#{STORAGE_TEST_FILE_PATH}.orig" do
   user node[:storage_test][:s3][:user]
   key node[:storage_test][:s3][:key]
-  container STORAGE_TEST_CONTAINER
-  object_name STORAGE_TEST_OBJECT_NAME 
-  provider_type STORAGE_TEST_PROVIDER
+  container "#{STORAGE_TEST_CONTAINER}"
+  object_name "#{STORAGE_TEST_OBJECT_NAME}" 
+  #provider "#{STORAGE_TEST_PROVIDER}"
   action :put
 end
 
@@ -30,14 +30,24 @@ remote_object_store "#{STORAGE_TEST_FILE_PATH}.new" do
   key node[:storage_test][:s3][:key]
   container STORAGE_TEST_CONTAINER
   object_name STORAGE_TEST_OBJECT_NAME  
-  provider_type STORAGE_TEST_PROVIDER
-  action :put
+#  provider STORAGE_TEST_PROVIDER
+  action :get
+end
+
+# remove file from S3
+remote_object_store "remove file" do
+  user node[:storage_test][:s3][:user]
+  key node[:storage_test][:s3][:key]
+  container STORAGE_TEST_CONTAINER
+  object_name STORAGE_TEST_OBJECT_NAME  
+#  provider STORAGE_TEST_PROVIDER
+  action :delete
 end
 
 # compare files
 ruby "test for identical files" do
   code <<-EOH
-    diff "#{STORAGE_TEST_FILE_PATH}.orig" "#{STORAGE_TEST_FILE_PATH}.new"
+    `diff "#{STORAGE_TEST_FILE_PATH}.orig" "#{STORAGE_TEST_FILE_PATH}.new"`
     raise "ERROR: files do not match!!" if $? != 0
   EOH
 end
