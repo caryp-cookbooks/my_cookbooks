b256a803361077fb50a87d84575005c080eede09
caryp <cary@rightscale.com>
2009-11-23 17:55:23 -0800

    lvm_test: new cookbook

---
+++ cookbooks/lvm_test/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "RightScale, Inc."
+maintainer_email "cary@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures lvm_test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
