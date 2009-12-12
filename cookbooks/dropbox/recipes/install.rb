57551cadd48f0e0df1e8120df96b28dd17189743
caryp <cary@rightscale.com>
2009-12-11 16:18:23 -0800

    dropbox: added instance registration

--- cookbooks/dropbox/recipes/install.rb
+++ cookbooks/dropbox/recipes/install.rb
@@ -1,3 +1,5 @@
+OUTPUT_FILE = "~/dropbox.log"
+
 bash "install dropbox" do
   code <<-EOH
     # Taken from http://wiki.dropbox.com/TipsAndTricks/TextBasedLinuxInstall
@@ -8,9 +10,20 @@ bash "install dropbox" do
     tar zxof dropbox.tar.gz
     wget http://dl.getdropbox.com/u/6995/dbmakefakelib.py
     wget http://dl.getdropbox.com/u/6995/dbreadconfig.py
-    ~/.dropbox-dist/dropboxd > ~/dropbox-register.log &
+    ~/.dropbox-dist/dropboxd > #{OUTPUT_FILE} &
     echo `cut & paste URL into a browser to register instance.
     sleep 10
     cat ~/dropbox-register.log
   EOH
 end
+
+ruby_bock "register instance" do
+  block do
+    link_line = `grep "link this machine" #{OUTPUT_FILE}`
+    words = link_line.split
+    url = words[2]
+    Chef::Log.info "Registering instance using URL: #{url}"
+    `curl #{url}`
+  end
+end
+
