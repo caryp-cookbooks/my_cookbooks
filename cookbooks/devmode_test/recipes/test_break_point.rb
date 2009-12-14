94a18a104ddfb45917f9785f6aee6b9a4e1c6e8d
caryp <cary@rightscale.com>
2009-12-14 15:00:09 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/recipes/test_break_point.rb
+++ cookbooks/devmode_test/recipes/test_break_point.rb
@@ -1,15 +1,16 @@
 COLLECTION_NAME = "breakpoints"
 
-class Chef::Recipe
-  include RightScale::ServerCollection
-end
-
 server_collection COLLECTION_NAME do
 #  tags "rs_agent_dev:break_point=*"
   tags "*"
 end
 
-Chef::Log.info("CKP:server collection: #{get_collection(COLLECTION_NAME)}")
+ruby "debug" do
+  code <<-EOH
+    include RightScale::ServerCollection
+    Chef::Log.info("CKP:server collection: #{get_collection(COLLECTION_NAME)}")
+  EOH
+end
 
 
 # right_link_tag "set breakpoint" do
