d372bf39e79973ab8724d497a0f48eb4102061b4
caryp <cary@rightscale.com>
2009-12-14 15:42:43 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/libraries/devmode.rb
+++ cookbooks/devmode_test/libraries/devmode.rb
@@ -17,3 +17,19 @@ module RightScale
 
   end
 end
+
+class RightScale
+      class MyServerCollection
+    
+        def initialize(node)
+          super()
+          @node = node
+        end
+    
+        def get_collection(collection_name)
+          raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection].has_key?(collection_name)
+          return @node[:server_collection][collection_name]
+        end  
+
+      end
+    end
