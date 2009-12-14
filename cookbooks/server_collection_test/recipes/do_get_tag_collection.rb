dd827b1c7936e416e821e773f027e963635b0a70
Jeremy Deininger <jeremy@rightscale.com>
2009-12-14 15:33:28 -0800

    slight changes

--- cookbooks/server_collection_test/recipes/do_get_tag_collection.rb
+++ cookbooks/server_collection_test/recipes/do_get_tag_collection.rb
@@ -1,12 +1,13 @@
 server_collection "master_servers" do
-  tags [ "db:" ]
+  tags [ "rs_dbrepl:" ]
 end
 
 ruby_block "display tags" do
   block do
     Chef::Log.info "Displaying Tags..."
-    node[:server_collection]['master_servers'].each do |ms|
+    node[:server_collection][:master_servers].each do |ms|
       Chef::Log.info "FOUND TAG: #{ms.inspect}"
     end
+    raise "NOT ENOUGH TAGS FOUND #{node[:server_collection][:master_servers].size}" unless node[:server_collection][:master_servers].size > 0
   end
 end
--- cookbooks/server_collection_test/recipes/do_tag.rb
+++ cookbooks/server_collection_test/recipes/do_tag.rb
@@ -1,7 +1,7 @@
-active_tag = "db:master_active=#{Time.now.strftime("%Y%m%d%H%M%S")}"
+active_tag = "rs_dbrepl:master_active=#{Time.now.strftime("%Y%m%d%H%M%S")}"
 Chef::Log.info "Tagging server with #{active_tag}"
 right_link_tag active_tag
 
-unique_tag = "db:master_instance_uuid=#{node[:rightscale][:instance_uuid]}"
+unique_tag = "rs_dbrepl:master_instance_uuid=#{node[:rightscale][:instance_uuid]}"
 Chef::Log.info "Tagging server with #{unique_tag}"
 right_link_tag unique_tag
