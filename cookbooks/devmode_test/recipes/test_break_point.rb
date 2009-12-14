e05d70de455cf336ed589a70a333b48b611d38c3
caryp <cary@rightscale.com>
2009-12-14 15:39:04 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/recipes/test_break_point.rb
+++ cookbooks/devmode_test/recipes/test_break_point.rb
@@ -15,7 +15,7 @@ ruby_block "debug" do
         end
     
         def get_collection(collection_name)
-          raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection][collection_name]
+          raise "No server collection found with name = #{collection_name}" unless node[:server_collection] && node[:server_collection].has_key?(collection_name)
           return @node[:server_collection][collection_name]
         end  
 
