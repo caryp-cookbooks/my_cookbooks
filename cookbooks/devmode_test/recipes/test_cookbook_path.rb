153ee374f430c37f178849ae10b2eac7d690c366
caryp <cary@rightscale.com>
2009-12-14 14:15:09 -0800

    typos

--- cookbooks/devmode_test/recipes/test_cookbook_path.rb
+++ cookbooks/devmode_test/recipes/test_cookbook_path.rb
@@ -2,14 +2,14 @@ COOKBOOK_PATH = "~/my_cookbooks"
 already_run = ::File.exists?(COOKBOOK_PATH)
 
 # Add tag to instance
-right_link_tag "rs_agent:cookbooks_path=#{COOKBOOK_PATH}" do
+right_link_tag "rs_agent_dev:cookbooks_path=#{COOKBOOK_PATH}" do
   not_if already_run
 end
 
 # Copy test cookbooks to COOKBOOK_PATH
 ruby "copy this repo" do
   not_if already_run
-  code <--EOH
+  code <<-EOH
     cb_root = ::File.join(__FILE__, "..", "..")
     `mkdir #{COOKBOOK_PATH}`
     `cp -r \#{cb_root}/* #{COOKBOOK_PATH}`
