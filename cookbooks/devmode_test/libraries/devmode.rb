c16f0045df96b9ac996250cff42a0b7bd1fe1499
caryp <cary@rightscale.com>
2009-12-14 14:46:30 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/libraries/devmode.rb
+++ cookbooks/devmode_test/libraries/devmode.rb
@@ -5,49 +5,15 @@
 module RightScale
   module ServerCollection
     
-    @mutex        = Mutex.new
-    @loaded_event = ConditionVariable.new
-
+    
     def breakpoint_set?(instance_uuid)
       
     end
     
-    def get_collection(tags)
-        server_collection ||= {}
-        return unless tags && !tags.empty?
-        status = :pending
-        result = nil
-        @mutex.synchronize do
-          EM.next_tick do
-            # Create the timer in the EM Thread
-            @timeout_timer = EM::Timer.new(QUERY_TIMEOUT) do
-              @mutex.synchronize do
-                status = :failed
-                @loaded_event.signal
-              end
-            end
-          end
-          RightScale::RequestForwarder.query_tags(tags) do |r|
-            @mutex.synchronize do
-              if status == :pending
-                result = r
-                status = :succeeded
-                @timeout_timer.cancel
-                @timeout_timer = nil
-                @loaded_event.signal
-              end
-            end
-          end
-          @loaded_event.wait(@mutex)
-        end
-        if status == :succeeded && result
-          server_collection = collection = {}
-          result.each { |k, v| collection[k] = v[:tags] }
-        else
-          RightScale::RightLinkLog.debug("ServerCollection load failed. (timed out after #{QUERY_TIMEOUT}s)")
-        end
-        true
-      end
+    def get_collection(collection_name)
+      raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection][collection_name]
+      return @node[:server_collection][collection_name]
+    end  
 
   end
 end
