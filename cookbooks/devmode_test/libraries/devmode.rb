2458e22521dfbb4b9ac760d64aa1baa919471961
caryp <cary@rightscale.com>
2009-12-14 15:47:41 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/libraries/devmode.rb
+++ cookbooks/devmode_test/libraries/devmode.rb
@@ -5,31 +5,11 @@
 module RightScale
   module ServerCollection
     
-    
-    def breakpoint_set?(instance_uuid)
-      
-    end
-    
     def get_collection(collection_name)
-      raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection][collection_name]
+      raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection].has_key?(collection_name)
       return @node[:server_collection][collection_name]
     end  
 
   end
 end
 
-class RightScale
-      class MyServerCollection
-    
-        def initialize(node)
-          super()
-          @node = node
-        end
-    
-        def get_collection(collection_name)
-          raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection].has_key?(collection_name)
-          return @node[:server_collection][collection_name]
-        end  
-
-      end
-    end
--- cookbooks/devmode_test/recipes/test_break_point.rb
+++ cookbooks/devmode_test/recipes/test_break_point.rb
@@ -1,12 +1,29 @@
 COLLECTION_NAME = "breakpoints"
 
 server_collection COLLECTION_NAME do
-#  tags "rs_agent_dev:break_point=*"
-  tags "rs_agent_dev:"
+  tags "rs_agent_dev:break_point=devmode_test::test_should_never_run"
 end
 
 ruby_block "debug" do
   block do
+    class RightScale
+      class MyServerCollection
+    
+        def initialize(node)
+          super()
+          @node = node
+        end
+        
+        def 
+    
+        def get_collection(collection_name)
+          raise "No server collection found with name = #{collection_name}" unless @node[:server_collection] && @node[:server_collection].has_key?(collection_name)
+          return @node[:server_collection][collection_name]
+        end  
+
+      end
+    end
+    
     collection = RightScale::MyServerCollection.new(node)
     
     Chef::Log.info("CKP:server collection: #{collection.get_collection('breakpoints').inspect}")
