f29a1d4ad55938c0360a994ad205681dc91dbb89
caryp <cary@rightscale.com>
2009-12-15 09:43:15 -0800

    dropbox: platform dectect fix

--- cookbooks/dropbox/recipes/install.rb
+++ cookbooks/dropbox/recipes/install.rb
@@ -1,12 +1,13 @@
 OUTPUT_FILE = "~/dropbox.log"
-
+platform = `uname -m` 
+suffix = (platform == "x86_64") ? platform : "x86"
+    
 bash "install dropbox" do
   code <<-EOH
     # Taken from http://wiki.dropbox.com/TipsAndTricks/TextBasedLinuxInstall
     #
     cd ~
-    platform=`uname -m` 
-    wget -O dropbox.tar.gz http://www.getdropbox.com/download?plat=lnx.$platform
+    wget -O dropbox.tar.gz http://www.getdropbox.com/download?plat=lnx.#{suffix}
     tar zxof dropbox.tar.gz
     wget http://dl.getdropbox.com/u/6995/dbmakefakelib.py
     wget http://dl.getdropbox.com/u/6995/dbreadconfig.py
