0ecead2e42a9e77caa58f50a4b4e7c2692bbc69b
caryp <cary@rightscale.com>
2009-12-14 18:18:14 -0800

    devmode_test: debugging

--- cookbooks/devmode_test/recipes/test_cookbook_path.rb
+++ cookbooks/devmode_test/recipes/test_cookbook_path.rb
@@ -1,15 +1,15 @@
-COOKBOOK_PATH = "\/root\/my_cookbooks"
+COOKBOOK_PATH = "/root/my_cookbooks"
 already_run = ::File.directory?(COOKBOOK_PATH)
 Chef::Log.info "Custom cookbook path exists = #{already_run}"
 
 # Add tag to instance
 right_link_tag "rs_agent_dev:cookbooks_path=#{COOKBOOK_PATH}" do
-  not_if already_run
+  not_if do already_run end
 end
 
 # Copy test cookbooks to COOKBOOK_PATH
 ruby "copy this repo" do
-  not_if already_run
+  not_if do already_run end
   code <<-EOH
     `mkdir #{COOKBOOK_PATH}`
     `cp -r #{::File.join(File.dirname(__FILE__), "..", "..", "..","*")} #{COOKBOOK_PATH}`
@@ -18,7 +18,7 @@ end
 
 # reboot
 # ruby_block "reboot" do
-#   not_if already_run
+#  not_if do already_run end
 #   block do
 #     `init 6`
 #   end
