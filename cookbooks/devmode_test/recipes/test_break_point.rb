fdcafb394721e0cbf8082aa6e84a8afefbc944f5
caryp <cary@rightscale.com>
2009-12-16 11:30:07 -0800

    debug

--- cookbooks/devmode_test/recipes/test_break_point.rb
+++ cookbooks/devmode_test/recipes/test_break_point.rb
@@ -19,7 +19,7 @@ ruby_block "Query for breakpoint" do
     tags = h[h.keys[0]]
     Chef::Log.info("CKP tags: #{tags.inspect}")
     result = tags.select { |s| s == TAG }
-    Chef::Log.info("CKP result: #{tag.inspect}")
+    Chef::Log.info("CKP result: #{result.inspect}")
     unless result.empty?
       Chef::Log.info("  Tag found!")
       node[:devmode_test][:has_breakpoint] = true
