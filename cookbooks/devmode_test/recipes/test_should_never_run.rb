4e3c6bda53633967757159024c3e45033504e2c3
caryp <cary@rightscale.com>
2009-12-15 17:23:21 -0800

    cleanup

--- cookbooks/devmode_test/recipes/test_should_never_run.rb
+++ cookbooks/devmode_test/recipes/test_should_never_run.rb
@@ -2,6 +2,7 @@
 # if the breakpoint is working this recipe will never be executed.
 
 ruby_block "I fail" do
+  only_if not_if do node[:devmode_test][:has_breakpoint] end
   block do
     raise "ERROR: If you intended to set a breakpoint -- it failed."
   end
