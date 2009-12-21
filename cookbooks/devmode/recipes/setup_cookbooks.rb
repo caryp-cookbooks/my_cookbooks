dae6595cd79d24d214fa5b56dbf36de4b82b5c0f
caryp <cary@rightscale.com>
2009-12-20 20:56:01 -0800

    devmode: debug

--- cookbooks/devmode/recipes/setup_cookbooks.rb
+++ cookbooks/devmode/recipes/setup_cookbooks.rb
@@ -61,10 +61,10 @@ right_link_tag TAG do
   not_if do node[:devmode_test][:loaded_custom_cookbooks] end
 end
 
-# # ..reboot!
-# ruby_block "reboot" do
-#  not_if do node[:devmode_test][:loaded_custom_cookbooks] end
-#   block do
-#     `init 6`
-#   end
-# end
+# ..reboot!
+ruby_block "reboot" do
+ not_if do node[:devmode_test][:loaded_custom_cookbooks] end
+  block do
+    `init 6`
+  end
+end
