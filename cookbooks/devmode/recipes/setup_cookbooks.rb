8f5bac7ec3bdb47ed26f082626a9572e5074a5cf
caryp <cary@rightscale.com>
2009-12-22 23:08:14 -0800

    devmode: developing

--- cookbooks/devmode/recipes/setup_cookbooks.rb
+++ cookbooks/devmode/recipes/setup_cookbooks.rb
@@ -87,10 +87,10 @@ ruby_block "hack provider with a dynamic tag name" do
 end
 
 # only reboot if cookbook_path.txt is found!
-# ruby_block "reboot" do
-#   not_if do node[:devmode_test][:loaded_custom_cookbooks] end
-#   only_if do ::File.exists?(COOKBOOK_FILE) end
-#   block do
-#     `init 6`
-#   end
-# end
+ruby_block "reboot" do
+  not_if do node[:devmode_test][:loaded_custom_cookbooks] end
+  only_if do ::File.exists?(COOKBOOK_FILE) end
+  block do
+    `init 6`
+  end
+end
