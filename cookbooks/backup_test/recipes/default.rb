7cb6deb2892c5db0d33648727c52d95d58f83ba6
caryp <cary@rightscale.com>
2010-01-06 15:23:03 -0800

    backup test removed

--- cookbooks/backup_test/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: backup_test
-# Recipe:: default
-#
-# Copyright 2009, RightScale, Inc.
-#
-# All rights reserved - Do Not Redistribute
-#
-include_recipe "backup_test::test_all"
