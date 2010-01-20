8d1cb9dd5c7684e8e13821d894bcba7a371d085e
caryp <cary@rightscale.com>
2010-01-20 10:43:17 -0800

    update metadata

--- cookbooks/state_test/metadata.rb
+++ cookbooks/state_test/metadata.rb
@@ -5,6 +5,9 @@ description      "Installs/Configures state_test"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
+recipe "state_test::default", "Run this as a boot script."
+recipe "state_test::check_value", "Run this as a operational script."
+
 attribute "state_test/value",
   :display_name => "Value that should be overwrite test recipe value",
   :recipes => [ "state_test::default" ]
