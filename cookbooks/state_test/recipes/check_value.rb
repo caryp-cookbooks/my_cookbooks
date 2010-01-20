75985dedd44b05734f6809338b00da3679fcf5e0
caryp <cary@rightscale.com>
2010-01-20 11:49:00 -0800

    added logging

--- cookbooks/state_test/recipes/check_value.rb
+++ cookbooks/state_test/recipes/check_value.rb
@@ -2,7 +2,9 @@ ruby_block "check value" do
   block do
     expected = "recipe"
     actual = node[:state_test][:value]
-    error = "ERROR: the node state is not persisted correctly between runs. Expected: #{expected} Actual: #{actual}"
+    Chef::Log.info "state_test::check_value -- Expected: #{expected} Actual: #{actual}"
+    error = "ERROR: the node state is not persisted correctly between runs."
     raise error unless expected == actual 
+    Chef::Log.info "state_test::check_value - PASS"
   end
 end
