f8ca50a1fa76643f9c52cdfbb548fefed85c6f66
Jeremy Deininger <jeremy@rightscale.com>
2010-01-25 23:54:12 -0800

    adding mysql_chef_premium features

---
+++ features/mysql_chef_premium.feature
@@ -0,0 +1,10 @@
+Feature: mysql_db premium resources and master/slave cluster operations
+  Tests the RightScale premium ServerTemplate Mysql Chef (alpha)
+
+  Scenario: Basic cluster failover operations
+    Given A deployment named "Regression Test CHEF - MySQL Multi-Cloud -JDDEV"
+    And 2 operational servers named "set1"
+    Then I should successfully run a recipe named "db_mysql::do_restore_and_become_master" on server 1. 
+    Then I should successfully run a recipe named "db_mysql::do_init_slave" on server 2.
+    Then I should successfully run a recipe named "db_mysql::do_promote_to_master" on server 2.
+    
--- features/step_definitions/cookbook_steps.rb
+++ features/step_definitions/cookbook_steps.rb
@@ -15,7 +15,19 @@ Given /A server named "(.*)"/ do |server|
   raise "FATAL: couldn't find a server named #{server}" unless server
 end
 
+Given /(\d+) operational servers named "(.*)"/ do |num, server|
+  servers = @deployment.servers_no_reload
+  @servers = servers.select { |s| s.nickname =~ /#{server}/ }
+  raise "need at least #{num} servers to start, only have: #{@servers.size}" if @servers.size < num.to_i
+  @servers.each { |s| s.start } 
+  @servers.each { |s| s.wait_for_operational_with_dns } 
+end
+
 Then /I should successfully run a recipe named "(.*)"/ do |recipe|
   @server.run_recipe(recipe)
+end 
+
+Then /I should successfully run a recipe named "(.*)" on server (\d+)/ do |recipe, server_index|
+  @servers[server_index].run_recipe(recipe)
 end 
 
