c6ec6f9a90366c98b8374e6a182b9e45d68e10c4
Jeremy Deininger <jeremy@rightscale.com>
2010-02-01 12:13:05 -0800

    adding from scratch feature, for mysql chef alpha, adding additional cookbook_step helpers

--- features/step_definitions/cookbook_steps.rb
+++ features/step_definitions/cookbook_steps.rb
@@ -36,4 +36,14 @@ Then /^I should sleep (\d+) seconds\.$/ do |seconds|
   sleep seconds.to_i
 end
 
+Then /^I should run a command "([^\"]*)" on server "([^\"]*)"\.$/ do |command, server_index|
+  human_index = server_index.to_i - 1
+  @servers[human_index].spot_check(command) { |result| puts result }
+end
+
+Then /^I should run a mysql query "([^\"]*)" on server "([^\"]*)"\.$/ do |query, server_index|
+  human_index = server_index.to_i - 1
+  query_command = "echo -e '#{query}'|mysql"
+  @servers[human_index].spot_check(query_command) { |result| puts result }
+end
 
