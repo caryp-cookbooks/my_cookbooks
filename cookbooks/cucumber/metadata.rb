341554924f69262aaa904081d4a94ebb4d9d5b28
caryp <cary@rightscale.com>
2010-02-12 15:55:01 -0800

    rake metadata

--- cookbooks/cucumber/metadata.rb
+++ cookbooks/cucumber/metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures cucumber"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
-provides cucumber_run_features(cwd, user, tags, options)
+provides "cucumber_run_features(:cwd, :user, :tags, :options)"
