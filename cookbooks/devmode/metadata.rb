1b0d614321a7ee6fdc60310c623b486d1c19719e
caryp <cary@rightscale.com>
2010-02-17 19:10:07 -0800

    devmode: added do_recipe_loop test recipe.

--- cookbooks/devmode/metadata.rb
+++ cookbooks/devmode/metadata.rb
@@ -4,3 +4,14 @@ license          "All rights reserved"
 description      "Installs/Configures devmode"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+attribute "devmode/recipe_loop/count",
+  :display_name => "Recipe Loop Count",
+  :default => "10",
+  :recipes => [ "devmode::do_recipe_loop" ]
+
+attribute "devmode/recipe_loop/recipe_name",
+  :display_name => "Recipe Name",
+  :description => "The name of the recipe to run in a loop. Syntax: <cookbook>::<recipe>",
+  :default => "10",
+  :recipes => [ "devmode::do_recipe_loop" ]
