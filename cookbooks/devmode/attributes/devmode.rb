23f977ac75a7b0b3a8941ef2a2e18ef2324dfa08
caryp <cary@rightscale.com>
2010-02-17 22:53:27 -0800

    devmode: debug

--- cookbooks/devmode/attributes/devmode.rb
+++ cookbooks/devmode/attributes/devmode.rb
@@ -1,6 +1,6 @@
 
 default[:devmode][:loaded_custom_cookbooks] = false
-
 default[:devmode][:recipe_loop][:count] = 1
-default[:devmode][:recipe_loop][:total] = 10
+
+set_unless[:devmode][:recipe_loop][:total] = 15
 default[:devmode][:recipe_loop][:recipe_name] = nil
