e0f86a0b5eec19fb25e0caaf7e61e473c3d6e0dc
caryp <cary@rightscale.com>
2010-02-17 20:56:19 -0800

    devmode: debug

--- cookbooks/devmode/metadata.rb
+++ cookbooks/devmode/metadata.rb
@@ -8,10 +8,10 @@ version          "0.1"
 attribute "devmode/recipe_loop/total",
   :display_name => "Recipe Loop Count",
   :default => "10",
-  :recipes => [ "devmode::do_recipe_loop" ]
+  :recipes => [ "devmode::do_recipe_loop", "devmode::do_recipe_loop_step" ]
 
 attribute "devmode/recipe_loop/recipe_name",
   :display_name => "Recipe Name",
   :description => "The name of the recipe to run in a loop. Syntax: <cookbook>::<recipe>",
   :required => true,
-  :recipes => [ "devmode::do_recipe_loop" ]
+  :recipes => [ "devmode::do_recipe_loop", "devmode::do_recipe_loop_step" ]
