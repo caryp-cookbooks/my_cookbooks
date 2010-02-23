1affd7fd03a8ff7c358d2fd0061647ce5fb4827f
caryp <cary@rightscale.com>
2010-02-23 15:21:33 -0800

    devmode: re-converge test change

--- cookbooks/devmode/recipes/do_converge_loop.rb
+++ cookbooks/devmode/recipes/do_converge_loop.rb
@@ -1,5 +1,7 @@
 RETURN_RECIPE = "devmode::do_converge_loop_step"
 
+include_recipe node[:devmode][:converge_loop][:recipe_name]
+
 devmode_converge_loop RETURN_RECIPE do
   remote_recipe RETURN_RECIPE
 end
