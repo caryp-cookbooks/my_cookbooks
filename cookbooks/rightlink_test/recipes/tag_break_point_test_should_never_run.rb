56204dd584fa8df83305b5f2c5af3ccf43b8a6ea
caryp <cary@rightscale.com>
2010-02-24 12:47:30 -0800

    rightlink_test: only test breakpoint on first pass.

--- cookbooks/rightlink_test/recipes/tag_break_point_test_should_never_run.rb
+++ cookbooks/rightlink_test/recipes/tag_break_point_test_should_never_run.rb
@@ -2,6 +2,7 @@
 # if the breakpoint is working this recipe will never be executed.
 
 ruby_block "I fail" do
+  not_if do node[:devmode_test][:disable_breakpoint_test] end
   block do
     raise "ERROR: If you intended to set a breakpoint -- it failed."
   end
