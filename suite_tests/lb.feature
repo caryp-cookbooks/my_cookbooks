108384224688e074099256f556a63c889a23994a
Martin Rhoads <martin@rightscale.com>
2010-03-24 20:28:13 +0000

    rails and lb haprofxy tests

--- suite_tests/lb.feature
+++ suite_tests/lb.feature
@@ -12,18 +12,28 @@ Scenario: LB server test
 
   When I launch the appservers
   Then the appservers become operational
+  And I should see all app servers in the haproxy config
 
-  When I cross connect the frontends
-  Then the cross connect script completes successfully
-  And I should see all servers in the haproxy config
-  And I should see all servers being served from haproxy 
+#  When I cross connect the frontends
+#  Then the cross connect script completes successfully
+#  And I should see all servers in the haproxy config
+# This doesn't work - there are multiple mongrels per ip
+# TODO
+# Works for PHP - need to get it going for rails (and tomcat)
+#  And I should see all servers being served from haproxy 
 
   Given with a known OS
   When I restart haproxy on the frontends
   Then haproxy status should be good
 
-  When I restart apahce on all servers
-  Then apache status should be good
+  When I restart apache on the frontends
+  Then apache status should be good on the frontends
+
+#  When I restart apache on all servers
+#  Then apache status should be good
+
+#  When I restart mongrels on all servers
+#  Then mongrel status should be good
 
   When I force log rotation
   Then I should see "/mnt/log/httpd/haproxy.log.1"
--- suite_tests/step_definitions/lb.rb
+++ suite_tests/step_definitions/lb.rb
@@ -1,4 +1,5 @@
 require "rubygems"
+require "ruby-debug"
 require "rest_connection"
 require "net/ssh"
 
@@ -12,8 +13,9 @@ When /^I cross connect the frontends$/ do
   puts "entering :I cross\-connect the frontends"
   @statuses = Array.new 
   st = ServerTemplate.find(@frontends.first.server_template_href)
-  script_to_run = st.executables.detect { |ex| ex.name =~  /LB app to HA proxy connect/i }
-  @frontends.each { |s| @statuses << s.run_script(script_to_run) }
+  script_to_run = st.executables.detect { |ex| ex.name =~  /LB [app|mongrels]+ to HA proxy connect/i }
+  raise "Could not find script" unless script_to_run
+  @frontends.each { |s| @statuses << s.run_executable(script_to_run) }
   puts "exiting :I cross\-connect the frontends"
 end
 
@@ -26,16 +28,25 @@ Then /^I should see all servers in the haproxy config$/ do
   @app_server_ips = Array.new
   @appservers.each { |app| @app_server_ips << app['private-ip-address'] }
   @frontends.each { |fe| @app_server_ips << fe['private-ip-address'] }
-
   @frontends.each do |fe|
     fe.settings
     #fe.reload
     haproxy_config = fe.spot_check_command('cat /home/haproxy/rightscale_lb.cfg | grep server')
-    @app_server_ips.each { |ip| haproxy_config.to_s.should include(ip) }
+    @app_server_ips.each { |ip|  haproxy_config.to_s.should include(ip) }
   end
- 
   puts "Exiting from :I should see all servers in the haproxy config"
+end
 
+Then /^I should see all app servers in the haproxy config$/ do
+  puts "entering :I should see all app servers in the haproxy config"
+  @app_server_ips = Array.new
+  @appservers.each { |app| @app_server_ips << app['private-ip-address'] }
+  @frontends.each do |fe|
+    fe.settings
+    haproxy_config = fe.spot_check_command('cat /home/haproxy/rightscale_lb.cfg | grep server')
+    @app_server_ips.each { |ip|  haproxy_config.to_s.should include(ip) }
+  end
+  puts "Exiting from :I should see all app servers in the haproxy config"
 end
 
 Then /^I should see all servers being served from haproxy$/ do
@@ -74,24 +85,65 @@ Then /^haproxy status should be good$/ do
   end
 end
 
-When /^I restart apahce on all servers$/ do
-  puts "entering :I restart apahce on all servers"
+When /^I restart apache on all servers$/ do
+  puts "entering :I restart apache on all servers"
   @statuses = Array.new
   st = ServerTemplate.find(@servers.first.server_template_href)
   script_to_run = st.executables.detect { |ex| ex.name =~  /WEB apache \(re\)start v2/i }
-  @frontends.each { |s| @statuses << s.run_script(script_to_run) }
-  puts "exiting :I restart apahce on all servers"
+  raise "Script not found" unless script_to_run
+  @frontends.each { |s| @statuses << s.run_executable(script_to_run) }
+  @appservers.each { |s| @statuses << s.run_executable(script_to_run) }
+  puts "exiting :I restart apache on all servers"
 end
 
 Then /^apache status should be good$/ do
   if @servers_os.first == "ubuntu"
-    apache_status_cmd = "service apache2 status"
+    apache_status_cmd = "apache2ctl status"
   else
     apache_status_cmd = "service httpd status"
   end
   @servers.each_with_index do |server,i|
     response = server.spot_check_command?(apache_status_cmd)
     raise "Apache status failed" unless response
+  end
+end
+
+When /^I restart apache on the frontends$/ do
+  puts "entering :I restart apache on the frontends"
+  @statuses = Array.new
+  st = ServerTemplate.find(@frontends.first.server_template_href)
+  script_to_run = st.executables.detect { |ex| ex.name =~  /WEB apache \(re\)start v2/i }
+  raise "Script not found" unless script_to_run
+  @frontends.each { |s| @statuses << s.run_executable(script_to_run) }
+  puts "exiting :I restart apache on the frontends"
+end
+
+Then /^apache status should be good on the frontends$/ do
+  if @servers_os.first == "ubuntu"
+    apache_status_cmd = "apache2ctl status"
+  else
+    apache_status_cmd = "service httpd status"
+  end
+  @frontends.each_with_index do |server,i|
+    response = server.spot_check_command?(apache_status_cmd)
+    raise "Apache status failed" unless response
+  end
+end
+
+When /^I restart mongrels on all servers$/ do
+  puts "entering :I restart mongrels on all servers"
+  @statuses = Array.new
+  st = ServerTemplate.find(@servers.first.server_template_href)
+  script_to_run = st.executables.detect { |ex| ex.name =~  /RB mongrel_cluster \(re\)start v1/i }
+  raise "Script not found" unless script_to_run
+  @frontends.each { |s| @statuses << s.run_executable(script_to_run) }
+  puts "exiting :I restart mongrels on all servers"
+end
+
+Then /^mongrel status should be good$/ do
+  @servers.each_with_index do |server,i|
+    response = server.spot_check_command?("service mongrel_cluster status")
+    raise "Mongrel status failed" unless response
   end
 end
 
@@ -115,6 +167,6 @@ Then /^the lb tests should succeed$/ do
     When I cross connect the frontends
     Then the cross connect script completes successfully
     And I should see all servers in the haproxy config
-    And I should see all servers being served from haproxy
+#    And I should see all servers being served from haproxy
   }
 end
