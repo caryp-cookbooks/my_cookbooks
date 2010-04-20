349a7727b775284f9dee155f83f31d7c6e6c989e
Jeremy <jeremy@rightscale.com>
2010-04-20 21:52:58 +0000

    cleaning up the cruft

--- vmonk/mysql_5.0_EBS_v2_beta_rev10.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'rest_connection'
-require File.dirname(__FILE__) + '/lib/deployment_monk'
-require 'ruby-debug'
-
-dm = DeploymentMonk.new([51712,51712])
-dm.generate_variations
-debugger
-puts 'blah'
