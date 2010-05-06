d263db90f8ac658bb238fb61b0797ee8441d6939
caryp <cary@rightscale.com>
2010-05-05 17:10:06 -0700

    vmonkey inputs cleanup

--- cookbooks/virtual_monkey/recipes/default.rb
+++ cookbooks/virtual_monkey/recipes/default.rb
@@ -6,14 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "virtual_monkey::setup_apptest_database"
-
 ["postfix", "mutt", "mailutils"].each { |p| package p }
 
-# grab application source from remote repository
-repo_git_pull "Get Test Repository" do
-  url @node[:virtual_monkey][:code][:url]
-  branch @node[:virtual_monkey][:code][:branch] 
-  dest "/root/test"
-  cred @node[:virtual_monkey][:code][:credentials]
-end
+include_recipe "virtual_monkey::setup_apptest_database"
+include_recipe "virtual_monkey::update_test_code"
+include_recipe "virtual_monkey::do_tests"
