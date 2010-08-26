60b55e97a30b77cce4d0af7fe56a8b600b18679b
Martin Rhoads <martin@rightscale.com>
2010-08-26 16:43:51 -0700

    adding build script for vhd-util

--- cookbooks/right_image_creator/recipes/install_vhd-util.rb
+++ cookbooks/right_image_creator/recipes/install_vhd-util.rb
@@ -1,1 +1,23 @@
-%w{mercurial git}.each do |p| package p end
+%w{mercurial git ncurses-devel dev86 iasl SDL python-devel libgcrypt-devel uuid-devel openssl-devel}.each do |p| package p end
+
+cookbook_file "/tmp/vhd-util-patch" do 
+  source "/tmp/vhd-util-patch"
+end
+
+bash "install_vhd-util" do 
+  script = <<EOF
+#!/bin/bash -ex
+    set -e
+    set -x
+
+    rm -rf /mnt/vhd && mkdir /mnt/vhd && cd /mnt/vhd
+    hg clone http://xenbits.xensource.com/xen-4.0-testing.hg
+    cd xen-4.0-testing.hg/tools
+    patch -p0 << /tmp/vhd-util-patch
+    cd ..
+    make install-tools
+    cd tools/blktap2/
+    make 
+    make install
+EOF
+end
