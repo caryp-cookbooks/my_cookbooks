1c753c507dd0ebae030c949728ded4bb789658fe
Martin Rhoads <martin@rightscale.com>
2010-09-09 16:44:18 -0700

    change how we set root passwd in dev mode

--- cookbooks/right_image_creator/recipes/bootstrap_centos.rb
+++ cookbooks/right_image_creator/recipes/bootstrap_centos.rb
@@ -232,8 +232,8 @@ end
 bash "setup_debug" do 
   only_if { node[:right_image_creator][:debug] == "true"  }
   code <<-EOH
-    sed -i 's%root::%root:$1$U.o7Mbpb$klvFnMZ4/LbM2xbyyfDlj1:%' #{node[:right_image_creator][:mount_dir]}/etc/shadow
-    sed -i 's%root:*:%root:$1$Yw4FVTSj$.OOi42zRornqDqchZ0J/y/:%' #{node[:right_image_creator][:mount_dir]}/etc/shadow
+    ## set root passwd to 'rightscale'
+    echo "root:rightscale" | chpasswd 
     echo 'PermitRootLogin yes' >> #{node[:right_image_creator][:mount_dir]}/etc/ssh/sshd_config
     echo 'PasswordAuthentication yes' >> #{node[:right_image_creator][:mount_dir]}/etc/ssh/sshd_config
 EOH
